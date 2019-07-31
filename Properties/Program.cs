33ae22a144e3e81d1a4888a308294eaca180ffd5
mbelnf1 <mbeln1992@gmail.com>
2019-07-31 15:56:09 -0400

    Add project files.

---
+++ Properties/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        // type "prop" + 2 tabs
+        public string LastName { get; set; }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} (1}", FirstName, LastName);
+            }
+        }
+    }
+}
